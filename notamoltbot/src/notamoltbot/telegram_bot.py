c48f12b441166e630b9bcf2ad77c9f87704c53a1
Patrick Dornian <patrick.dornian@gmail.com>
2026-02-07 16:56:10 -0700

    reorg to package structure using uv

--- notamoltbot/src/notamoltbot/telegram_bot.py
+++ notamoltbot/src/notamoltbot/telegram_bot.py
@@ -23,8 +23,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
